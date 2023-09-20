52d59b1052cf186e68e9e2ec912763c22cd04c2f
João Michelotti <joaovitorbulegon@hotmail.com>
2023-09-20 20:46:06 -0300

    Finalizado porem nao vem em ordem

--- scripts.js
+++ scripts.js
@@ -1,25 +1,44 @@
 var cont = 0;
 
+ordem = []
 
 for (var i = 1; i <= 200; i++) {
     cont++;
-    $.get(`https://pokeapi.co/api/v2/pokemon/${cont}`, preencherCard);
+    
+    $.get(`https://pokeapi.co/api/v2/pokemon/${cont}`, preencherCard) 
 
+    
+    
+    
+ 
 }
 
 function preencherCard(data){
 
+    const types = data.types;
+
+    console.log(data.sprites.front_default)
+    
     var Html = ""
 
     Html = "<div class='main'> "
 
-    Html += "<div class='banner'> <img src='' alt=''> </div> <div class='body'> "
-    Html += `<h2>${data.name}</h2> <p>Tipo: Tipo do Pokémon</p> <p>Número da Pokédex: #001</p> </div> </div>`    
+    Html += "<div class='banner'>   "
+    Html += `<img src='${data.sprites.front_default}' alt='' </div> <div class='body'>`
+    Html += `<h2>${data.name}</h2> <p>Tipo: `    
+
+
+    for (var i = 0; i < types.length; i++) {
+        Html += `${types[i].type.name} `;
+    }
+
+    Html += `</p> <p>Número da Pokédex: #${data.id}</p> </div> </div>`
      $("#container").append(Html)
                
-
-    //console.log(data.name)
 }
+
+
+
 
 
 
